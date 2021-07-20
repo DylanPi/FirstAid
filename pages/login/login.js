a3bbd0051f559b27c98685abf631fc9a85d149ca
DylanPi <1356029300@qq.com>
2021-07-21 00:17:22 +0800

    add login function, ssome changes on tt component wxcc file

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,24 +1,80 @@
 // pages/login/login.js
-Page({
 
+const app = getApp()
+const AV = require('../../libs/av-core-min.js');
+
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-    completeRegist:false,
-    com_reg:""
+    completeRegist: false,
+    com_reg: "",
+    username: '',
+    password: ''
   },
 
+  /**
+   * 输入用户名监听
+   */
+  inputNameLog(e) {
+    // console.log(e.detail.value)
+    this.setData({
+      username: e.detail.value,
+    })
+  },
+
+  /**
+   * 输入密码监听
+   */
+  inputPwLog(e) {
+    // console.log(e.detail.value)
+    this.setData({
+      password: e.detail.value,
+    })
+  },
+
+  /**
+   * 登录事件
+   */
+  login() {
+    //   wx.navigateTo({
+    //     url: '../message/message?message='+this.data.username,
+    //   })
+    const {
+      username,
+      password,
+    } = this.data;
+    // console.log(username)
+    AV.User.logIn(username, password).then(function (loginedUser) {
+      // wx.redirectTo({
+      //   url: '../message/message?message=' + "hello world!",
+      // });
+      wx.showToast({
+        title: '登录成功',
+        icon: 'success',
+      })
+      // 登录成功，跳转到message页面
+    }, function (error) {
+      wx.redirectTo({
+          url: '../login/login',
+        });
+      wx.showToast({
+        title: '检查账号和密码',
+        icon: 'error',
+      });
+      alert(JSON.stringify(error));
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    if (this.data.completeRegist){
+    if (this.data.completeRegist) {
       this.setData(
-        this.data.com_reg="已完成注册，请登录!"
+        this.data.com_reg = "已完成注册，请登录!"
       )
     }
-
   },
 
   /**
@@ -49,24 +105,5 @@ Page({
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
 
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
